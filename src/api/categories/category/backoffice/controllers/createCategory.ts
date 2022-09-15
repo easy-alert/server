1992a76c9a63050f719f1159429278ad15d12284
Carlos Pasquali <carlosdpasqualip@hotmail.com>
2022-09-15 09:57:46 -0300

    ajuste

--- src/api/categories/category/backoffice/controllers/createCategory.ts
+++ src/api/categories/category/backoffice/controllers/createCategory.ts
@@ -7,24 +7,26 @@ import { NextFunction, Request, Response } from 'express';
 
 import { CategoryServices } from '../../services/categoryServices';
 import { Validator } from '../../../../../utils/validator/validator';
-import { ServerMessage } from '../../../../../utils/messages/serverMessage';
 
 const validator = new Validator();
 const categoryServices = new CategoryServices();
 
 export async function createCategory(
   req: Request,
-  _res: Response,
+  res: Response,
   _next: NextFunction,
 ) {
   const { name } = req.body;
 
   validator.notNull([{ label: 'nome da categoria', variable: name }]);
 
-  await categoryServices.create({ name });
+  const category = await categoryServices.create({ name });
 
-  throw new ServerMessage({
-    statusCode: 201,
-    message: 'Categoria cadastrada com sucesso.',
+  return res.status(200).json({
+    category,
+    ServerMessage: {
+      statusCode: 201,
+      message: 'Categoria cadastrada com sucesso.',
+    },
   });
 }
