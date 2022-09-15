e108e973b69683ae56fa7f9aef093d7c522788c3
Vitor Vassoler Bonfante <vitorbonfante@hotmail.com>
2022-09-15 19:16:35 +0000

    Update corsOptions.ts

--- src/middlewares/corsOptions.ts
+++ src/middlewares/corsOptions.ts
@@ -5,6 +5,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
+  'http://backoffice-easyalert-sandbox.s3-website-us-west-2.amazonaws.com'
 ];
 
 export const corsOptions: cors.CorsOptions = {
