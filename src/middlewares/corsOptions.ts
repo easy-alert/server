ac90b04fd0136408d889846c3950c239b4ee659c
Carlos Pasquali <carlosdpasqualip@hotmail.com>
2022-10-17 16:35:49 +0000

    Update src/middlewares/corsOptions.ts

--- src/middlewares/corsOptions.ts
+++ src/middlewares/corsOptions.ts
@@ -2,8 +2,8 @@ import cors from 'cors';
 
 // CHANGE HERE
 const allowedOrigins = [
-  'http://backoffice-easyalert-sandbox.s3-website-us-west-2.amazonaws.com/',
-  'http://backoffice-easyalert-production.s3-website-us-west-2.amazonaws.com/',
+  'http://backoffice-easyalert-sandbox.s3-website-us-west-2.amazonaws.com',
+  'http://backoffice-easyalert-production.s3-website-us-west-2.amazonaws.com',
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
