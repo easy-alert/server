95edc8d61c5b75096b74027257e7cc7012fdf8f0
Carlos Pasquali <carlosdpasqualip@hotmail.com>
2022-10-17 11:24:28 -0300

    fix auth

--- src/middlewares/corsOptions.ts
+++ src/middlewares/corsOptions.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 // CHANGE HERE
 const allowedOrigins = [
   'http://backoffice-easyalert-sandbox.s3-website-us-west-2.amazonaws.com/',
+  'http://backoffice-easyalert-production.s3-website-us-west-2.amazonaws.com/',
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
