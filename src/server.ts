11b3f4e2359348fb357ff669cc554d76617b74c8
Carlos Pasquali <carlosdpasqualip@hotmail.com>
2022-10-27 13:05:26 +0000

    Update src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import { App } from './app';
 
+//test
 new App().server.listen(process.env.PORT || 8080, () =>
   console.log(
     '\n\n\n 😎 Server is running 😎 \n\n',
