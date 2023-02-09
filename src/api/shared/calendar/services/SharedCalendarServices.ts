1a41bd1a7d6ae3b8378b825d5699df8d99ca166b
Carlos Pasquali <carlos.pasquali.dev@gmail.com>
2023-02-09 16:43:39 -0300

    SA-328 - fix calendar list

--- src/api/shared/calendar/services/SharedCalendarServices.ts
+++ src/api/shared/calendar/services/SharedCalendarServices.ts
@@ -48,6 +48,7 @@ export class SharedCalendarServices {
 
       prisma.maintenanceHistory.findMany({
         select: {
+          id: true,
           notificationDate: true,
 
           Building: {
