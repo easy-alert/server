1b3b878173378a88d6cb2156e7f4567a38a2c013
Jorge Andrade <jorgeluiz112233@gmail.com>
2023-02-22 17:53:45 +0000

    Update corsOptions.ts

--- src/middlewares/corsOptions.ts
+++ src/middlewares/corsOptions.ts
@@ -2,12 +2,12 @@ import cors from 'cors';
 
 // CHANGE HERE
 const allowedOrigins = [
-  'backoffice.easyalert.com.br',
-  'company.easyalert.com.br',
-  'public.easyalert.com.br',
-  'sandbox.backoffice.easyalert.com.br',
-  'sandbox.company.easyalert.com.br',
-  'sandbox.public.easyalert.com.br',
+  'https://backoffice.easyalert.com.br',
+  'https://company.easyalert.com.br',
+  'https://public.easyalert.com.br',
+  'https://sandbox.backoffice.easyalert.com.br',
+  'https://sandbox.company.easyalert.com.br',
+  'https://sandbox.public.easyalert.com.br',
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
