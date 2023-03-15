fb2e2351ace5f917a463e08ce8718bd3b9ff39c8
Jorge Andrade <jorgeluiz112233@gmail.com>
2023-03-15 18:57:57 +0000

    Update corsOptions.ts

--- src/middlewares/corsOptions.ts
+++ src/middlewares/corsOptions.ts
@@ -11,6 +11,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
+  'http://client.easyalert.com.br',
+  'https://app.easyalert.com.br'
 ];
 
 export const corsOptions: cors.CorsOptions = {
