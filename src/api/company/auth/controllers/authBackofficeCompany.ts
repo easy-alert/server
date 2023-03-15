a71f933cb8741c75de3e6831913edaf49e2968fc
Carlos Pasquali <carlos.pasquali.dev@gmail.com>
2023-03-15 17:21:27 -0300

    access -

---
+++ src/api/company/auth/controllers/authBackofficeCompany.ts
@@ -0,0 +1,55 @@
+// TYPES
+import { Response, Request } from 'express';
+
+// CLASS
+import { AuthServices } from '../../../shared/auth/services/authServices';
+import { TokenServices } from '../../../../utils/token/tokenServices';
+import { Validator } from '../../../../utils/validator/validator';
+
+import { PermissionServices } from '../../../shared/permission/services/permissionServices';
+
+const permissionServices = new PermissionServices();
+const authServices = new AuthServices();
+const tokenServices = new TokenServices();
+
+const validator = new Validator();
+
+export const authBackofficeCompany = async (req: Request, res: Response) => {
+  const { userId, backOfficeToken } = req.body;
+
+  validator.notNull([
+    { label: 'id do usuário', variable: userId },
+    { label: 'id do usuário', variable: backOfficeToken },
+  ]);
+
+  tokenServices.decode({ token: backOfficeToken });
+
+  const user = await authServices.findById({ userId });
+
+  await permissionServices.checkPermission({
+    UserPermissions: user.Permissions,
+    permission: 'Company',
+  });
+
+  const token = tokenServices.generate({
+    tokenData: {
+      userId: user.id,
+      Permissions: user.Permissions,
+      Company: user.Companies[0].Company,
+    },
+  });
+
+  return res.status(200).json({
+    User: {
+      id: user.id,
+      image: user.image,
+      name: user.name,
+      email: user.email,
+      lastAccess: user.lastAccess,
+      createdAt: user.createdAt,
+      Permissions: user.Permissions,
+    },
+    Company: user.Companies[0].Company,
+    token,
+  });
+};
