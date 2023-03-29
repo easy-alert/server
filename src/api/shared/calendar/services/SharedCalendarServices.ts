09a772d8ea08823be6d3d259e2b230c35ecd23f2
Carlos Pasquali <carlos.pasquali.dev@gmail.com>
2023-03-29 11:51:28 -0300

    develop -

--- src/api/shared/calendar/services/SharedCalendarServices.ts
+++ src/api/shared/calendar/services/SharedCalendarServices.ts
@@ -47,6 +47,7 @@ export class SharedCalendarServices {
       } else {
         dates.push({
           ...maintenanceData,
+
           notificationDate: date,
           isFuture,
           periodDaysInterval,
