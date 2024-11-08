f43093627ff06d9c6adbff738a2f103c4a267b49
Guilherme Roos <guilherme.roosr@gmail.com>
2024-11-07 23:44:07 -0300

    feat: add POST route for creating ticket history activities

--- src/api/client/ticketHistoryActivities/ticketHistoryActivities.routes.ts
+++ src/api/client/ticketHistoryActivities/ticketHistoryActivities.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 
 import { findTicketHistoryActivitiesById } from '../../shared/ticketHistoryActivities/controllers/findTicketHistoryActivitiesById';
+import { createTicketHistoryActivity } from '../../shared/ticketHistoryActivities/controllers/createTicketHistoryActivity';
 
 export const ticketHistoryActivitiesRouter: Router = Router();
 
 ticketHistoryActivitiesRouter.get('/:ticketId', findTicketHistoryActivitiesById);
+
+ticketHistoryActivitiesRouter.post('/', createTicketHistoryActivity);
