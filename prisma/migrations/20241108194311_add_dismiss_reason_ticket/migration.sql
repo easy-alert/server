f419f3ffbbccdc92f2600e3d6ed3ac6999c19591
Guilherme Roos <guilherme.roosr@gmail.com>
2024-11-13 14:43:00 -0300

    feat: update tickets table to include dismissedById and foreign key constraint

--- prisma/migrations/20241108194311_add_dismiss_reason_ticket/migration.sql
+++ prisma/migrations/20241108194311_add_dismiss_reason_ticket/migration.sql
@@ -2,5 +2,8 @@
 ALTER TABLE "tickets"
 ADD COLUMN     "seen"               BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN     "dismissObservation" TEXT,
-ADD COLUMN     "dismissedBy"        TEXT,
+ADD COLUMN     "dismissedById"      TEXT,
 ADD COLUMN     "dismissedAt"        TIMESTAMP(3);
+
+-- AddForeignKey
+ALTER TABLE "tickets" ADD CONSTRAINT "tickets_dismissedById_fkey" FOREIGN KEY ("dismissedById") REFERENCES "buildingsNotificationsConfigurations"("id") ON DELETE SET NULL ON UPDATE CASCADE;
