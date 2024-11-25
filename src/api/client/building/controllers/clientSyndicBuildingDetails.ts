7ee0ae98e10f8f9b262e0d283878ef93613d0963
Guilherme Roos <guilherme.roosr@gmail.com>
2024-11-25 15:50:09 -0300

    feat: include maintenance priority visibility in building details response

--- src/api/client/building/controllers/clientSyndicBuildingDetails.ts
+++ src/api/client/building/controllers/clientSyndicBuildingDetails.ts
@@ -222,6 +222,7 @@ export async function clientSyndicBuildingDetails(req: Request, res: Response) {
 
   return res.status(200).json({
     buildingName: buildingNotificationConfig.Building.name,
+    showPriority: company?.showMaintenancePriority,
     kanban,
     Filters,
   });
