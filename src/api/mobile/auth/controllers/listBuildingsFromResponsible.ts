ba68c9198c50664d303ad753801653997aad6505
Guilherme Roos <guilherme.roosr@gmail.com>
2024-12-16 15:32:25 -0300

    feat: enhance listBuildingsFromResponsible to support email and password authentication

--- src/api/mobile/auth/controllers/listBuildingsFromResponsible.ts
+++ src/api/mobile/auth/controllers/listBuildingsFromResponsible.ts
@@ -7,20 +7,35 @@ const authMobile = new AuthMobile();
 export async function listBuildingsFromResponsible(req: Request, res: Response) {
   try {
     // Obtém o phoneNumber da query string
-    const { phoneNumber } = req.query;
+    const { email, phoneNumber, password, createPassword } = req.body;
 
-    // Verifica se o phoneNumber foi enviado
-    if (!phoneNumber || typeof phoneNumber !== 'string') {
-      return res
-        .status(400)
-        .json({ error: 'O número de telefone é obrigatório e deve ser uma string.' });
+    if (createPassword) {
+      // Altera a senha do usuário
+      await authMobile.changePassword({ email, phoneNumber, password });
     }
 
-    // Chama o serviço para buscar os prédios
-    const buildings = await authMobile.listBuildings({ phoneNumber });
+    if ((email || phoneNumber) && password) {
+      // Verifica se o usuário pode logar
+      const user = await authMobile.login({ email, phoneNumber, password });
 
-    // Retorna os prédios encontrados
-    return res.status(200).json(buildings);
+      if (!user) {
+        return res.status(200).json({ error: 'Usuário não encontrado ou senha incorreta.' });
+      }
+
+      // Chama o serviço para buscar os prédios
+      const buildings = await authMobile.listBuildings({ phoneNumber });
+
+      return res.status(200).json({ buildings });
+    }
+
+    if (email || phoneNumber) {
+      // Verifica se o usuário pode logar
+      const { canLogin, hasPassword } = await authMobile.canLogin({ email, phoneNumber });
+
+      return res.status(200).json({ canLogin, hasPassword });
+    }
+
+    return res.status(400).json({ error: 'Parâmetros inválidos.' });
   } catch (error) {
     return res.status(500).json({ error: 'Erro interno do servidor.' });
   }
