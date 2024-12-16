b1a509f885bb886e3078da92dd3735a915a8a874
Guilherme Roos <guilherme.roosr@gmail.com>
2024-12-16 15:32:47 -0300

    feat: implement email and phone number authentication methods in AuthMobile service

--- src/api/mobile/auth/services/authMobile.ts
+++ src/api/mobile/auth/services/authMobile.ts
@@ -1,6 +1,99 @@
 import { prisma } from '../../../../../prisma';
 
 export class AuthMobile {
+  async canLogin({
+    email,
+    phoneNumber,
+  }: {
+    email: string;
+    phoneNumber: string;
+  }): Promise<{ canLogin: boolean; hasPassword: boolean }> {
+    const user = await prisma.buildingNotificationConfiguration.findFirst({
+      where: {
+        OR: [
+          {
+            email,
+          },
+          {
+            contactNumber: phoneNumber,
+          },
+        ],
+      },
+    });
+
+    return { canLogin: !!user, hasPassword: !!user?.password };
+  }
+
+  async login({
+    email,
+    phoneNumber,
+    password,
+  }: {
+    email: string;
+    phoneNumber: string;
+    password: string;
+  }) {
+    const user = await prisma.buildingNotificationConfiguration.findFirst({
+      where: {
+        OR: [
+          {
+            email,
+          },
+          {
+            contactNumber: phoneNumber,
+          },
+        ],
+
+        AND: {
+          password,
+        },
+      },
+    });
+
+    return user;
+  }
+
+  async changePassword({
+    email,
+    phoneNumber,
+    password,
+  }: {
+    email: string;
+    phoneNumber: string;
+    password: string;
+  }) {
+    const userList = await prisma.buildingNotificationConfiguration.findMany({
+      where: {
+        OR: [
+          {
+            email,
+          },
+          {
+            contactNumber: phoneNumber,
+          },
+        ],
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    if (!userList.length || !userList[0].id) {
+      throw new Error('Usuário não encontrado.');
+    }
+
+    userList.forEach(async (user) => {
+      await prisma.buildingNotificationConfiguration.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          password,
+        },
+      });
+    });
+  }
+
   async listBuildings({ phoneNumber }: { phoneNumber: string }) {
     const buildings = await prisma.buildingNotificationConfiguration.findMany({
       where: {
