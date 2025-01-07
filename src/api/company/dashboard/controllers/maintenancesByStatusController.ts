7404941e3dc6b9f56525b33c92a2839de0ef1bdc
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-07 17:34:32 -0300

    feat: add maintenancesByStatusController to retrieve maintenance status data

---
+++ src/api/company/dashboard/controllers/maintenancesByStatusController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+
+import { dashboardServices } from '../services/dashboardServices';
+
+import { getPeriod } from '../../../../utils/dateTime/getPeriod';
+import { handleDashboardFilter } from '../../../../utils/filters/handleDashboardFilter';
+
+export async function maintenancesByStatusController(req: Request, res: Response) {
+  const { period, buildings, categories, responsible } = req.query;
+  const { startDate, endDate } = getPeriod(period as string | undefined);
+
+  const dashboardFilter = handleDashboardFilter({
+    buildings: buildings as string | string[],
+    categories: categories as string | string[],
+    responsible: responsible as string | string[],
+    startDate,
+    endDate,
+    companyId: req.Company.id,
+  });
+
+  const maintenances = await dashboardServices.maintenancesByStatus({
+    filter: dashboardFilter,
+  });
+
+  const maintenancesStatusData: { data: number[]; labels: string[]; colors: string[] } = {
+    data: [
+      maintenances.completedMaintenances,
+      maintenances.expiredMaintenances,
+      maintenances.pendingMaintenances,
+    ],
+    labels: ['Concluídas', 'Vencidas', 'Pendentes'],
+    colors: ['#34B53A', '#FF3508', '#FFB200'],
+  };
+
+  return res.status(200).json({ ...maintenancesStatusData });
+}
