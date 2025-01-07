23cabfe130cd1c71ed74b987c980dd9dc46e2c85
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-07 17:36:47 -0300

    feat: add routes for maintenances and tickets data retrieval and calculations

--- src/api/company/dashboard/dashboard.routes.ts
+++ src/api/company/dashboard/dashboard.routes.ts
@@ -4,8 +4,22 @@ import { Router } from 'express';
 // FUNCTIONS
 import { listAuxiliaryData, listData } from './controllers';
 
+import { maintenancesCountAndCostController } from './controllers/MaintenancesCountAndCostController';
+import { maintenancesByStatusController } from './controllers/maintenancesByStatusController';
+import { maintenancesTimelineController } from './controllers/maintenancesTimelineController';
+
+import { ticketsCountAndCostController } from './controllers/TicketsCountAndCostController';
+import { ticketsByServiceTypeController } from './controllers/ticketsByServiceTypeController';
+
 // ROUTES
 export const dashboardRouter = Router();
 
 dashboardRouter.get('/list-data', listData);
 dashboardRouter.get('/list-auxiliary-data', listAuxiliaryData);
+
+dashboardRouter.get('/maintenances/count-and-cost', maintenancesCountAndCostController);
+dashboardRouter.get('/maintenances/status', maintenancesByStatusController);
+dashboardRouter.get('/maintenances/timeline', maintenancesTimelineController);
+
+dashboardRouter.get('/tickets/count-and-cost', ticketsCountAndCostController);
+dashboardRouter.get('/tickets/service-types', ticketsByServiceTypeController);
