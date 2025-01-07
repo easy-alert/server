2c23ddff82d6e975761aad406d4f95cd7327e690
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-07 17:34:55 -0300

    feat: add maintenancesCountAndCostController to calculate maintenance counts and costs

---
+++ src/api/company/dashboard/controllers/maintenancesCountAndCostController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+
+import { dashboardServices } from '../services/dashboardServices';
+
+import { getPeriod } from '../../../../utils/dateTime/getPeriod';
+import { handleDashboardFilter } from '../../../../utils/filters/handleDashboardFilter';
+import { mask } from '../../../../utils/masks';
+
+export async function maintenancesCountAndCostController(req: Request, res: Response) {
+  const { period, buildings, categories, responsible, maintenanceType } = req.query;
+  const { startDate, endDate } = getPeriod(period as string | undefined);
+
+  const dashboardFilter = handleDashboardFilter({
+    buildings: buildings as string | string[],
+    categories: categories as string | string[],
+    responsible: responsible as string | string[],
+    startDate,
+    endDate,
+    companyId: req.Company.id,
+  });
+
+  const countAndCostMaintenances = await dashboardServices.maintenancesCountAndCost({
+    filter: dashboardFilter,
+    maintenanceType: maintenanceType ? (maintenanceType as 'common' | 'occasional') : undefined,
+  });
+
+  const maintenancesCost = `Valor investido ${mask({
+    type: 'BRL',
+    value: String(countAndCostMaintenances.maintenancesCost),
+  })}`;
+
+  return res.status(200).json({ ...countAndCostMaintenances, maintenancesCost });
+}
