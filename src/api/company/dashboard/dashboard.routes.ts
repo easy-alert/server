bf209519be195bf506135dfc49026656edc272b3
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-08 16:23:14 -0300

    refactor: update dashboard routes to use new controller structure and endpoints

--- src/api/company/dashboard/dashboard.routes.ts
+++ src/api/company/dashboard/dashboard.routes.ts
@@ -1,25 +1,28 @@
 // LIBS
 import { Router } from 'express';
 
-// FUNCTIONS
-import { listAuxiliaryData, listData } from './controllers';
-
-import { maintenancesCountAndCostController } from './controllers/MaintenancesCountAndCostController';
-import { maintenancesByStatusController } from './controllers/maintenancesByStatusController';
-import { maintenancesTimelineController } from './controllers/maintenancesTimelineController';
-
-import { ticketsCountAndCostController } from './controllers/TicketsCountAndCostController';
-import { ticketsByServiceTypeController } from './controllers/ticketsByServiceTypeController';
+import {
+  dashboardFiltersController,
+  maintenancesByStatusController,
+  maintenancesCountAndCostController,
+  maintenancesMostCompletedExpiredController,
+  maintenancesTimelineController,
+  ticketsByServiceTypeController,
+  ticketsCountAndCostController,
+} from './controllers';
 
 // ROUTES
 export const dashboardRouter = Router();
 
-dashboardRouter.get('/list-data', listData);
-dashboardRouter.get('/list-auxiliary-data', listAuxiliaryData);
+dashboardRouter.get('/filters', dashboardFiltersController);
 
 dashboardRouter.get('/maintenances/count-and-cost', maintenancesCountAndCostController);
 dashboardRouter.get('/maintenances/status', maintenancesByStatusController);
 dashboardRouter.get('/maintenances/timeline', maintenancesTimelineController);
+dashboardRouter.get(
+  '/maintenances/most-completed-expired',
+  maintenancesMostCompletedExpiredController,
+);
 
 dashboardRouter.get('/tickets/count-and-cost', ticketsCountAndCostController);
 dashboardRouter.get('/tickets/service-types', ticketsByServiceTypeController);
