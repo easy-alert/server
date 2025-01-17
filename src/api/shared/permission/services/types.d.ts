0be317424da087d4d46137302a0b1dbf11a738fc
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-17 00:32:38 -0300

    refactor: move PermissionServices and types to new directory structure and add getAllPermissions method

--- src/api/shared/permission/services/types.d.ts
+++
@@ -1,6 +0,0 @@
-import type { TPermissionsNames } from '../../../../types/TPermissionsNames';
-
-export interface ICheckPermission {
-  UserPermissions: { Permission: { name: string } }[];
-  permissions: TPermissionsNames[];
-}
