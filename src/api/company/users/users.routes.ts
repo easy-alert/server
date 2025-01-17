af6fe62a381849586b66a4d42be28f4cf4ba460d
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-17 00:40:31 -0300

    feat: add findUserPermissions controller and route to retrieve user permissions

--- src/api/company/users/users.routes.ts
+++ src/api/company/users/users.routes.ts
@@ -3,9 +3,11 @@ import { Router } from 'express';
 
 // CONTROLLERS
 import { findUserByIdController } from './controllers/findUserByIdController';
+import { findUserPermissionsController } from './controllers/findUserPermissionsController';
 
 // ROUTES
 export const usersRouter = Router();
 
 usersRouter.get('/');
 usersRouter.get('/details', findUserByIdController);
+usersRouter.get('/permissions', findUserPermissionsController);
