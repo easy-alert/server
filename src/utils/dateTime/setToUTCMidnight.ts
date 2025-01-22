c2380a9bd1b3d54818d058f27f4e7e2f9be58cda
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-22 16:34:16 -0300

    feat: add newline for improved readability in setToUTCMidnight function

--- src/utils/dateTime/setToUTCMidnight.ts
+++ src/utils/dateTime/setToUTCMidnight.ts
@@ -1,14 +1,16 @@
 import { hyphenToSlash } from '../dataHandler';
-import { changeTime } from './changeTime';
+// import { changeTime } from './changeTime';
 
 export function setToUTCMidnight(date: Date | string) {
   let dateValue = date;
 
   if (typeof dateValue === 'string') {
     dateValue = hyphenToSlash(String(dateValue)).substring(0, 10);
+    dateValue = new Date(dateValue);
   }
 
-  const newDate = new Date(dateValue);
-  const midnight = changeTime({ date: newDate, time: { h: 0, m: 0, s: 0, ms: 0 } });
+  dateValue.setUTCHours(3, 0, 0, 0);
+  const midnight = dateValue;
+
   return midnight;
 }
