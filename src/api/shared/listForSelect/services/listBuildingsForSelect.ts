253e1c2adf5dc869ac2980e3b6e5b6e0539c426c
Guilherme Roos <guilherme.roosr@gmail.com>
2025-01-30 00:46:42 -0300

    feat: add listBuildingsForSelect function to retrieve building details for selection

---
+++ src/api/shared/listForSelect/services/listBuildingsForSelect.ts
@@ -0,0 +1,26 @@
+import { prisma } from '../../../../../prisma';
+
+interface IListBuildingsForSelect {
+  companyId: string;
+  buildingsIds?: string[];
+}
+
+export async function listBuildingsForSelect({ companyId, buildingsIds }: IListBuildingsForSelect) {
+  return prisma.building.findMany({
+    select: {
+      id: true,
+      nanoId: true,
+      name: true,
+    },
+
+    where: {
+      companyId,
+
+      AND: {
+        id: {
+          in: buildingsIds,
+        },
+      },
+    },
+  });
+}
