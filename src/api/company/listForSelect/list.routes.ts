b23cea6283cb8c90c8405d12e35e126664707cb6
Guilherme Roos <guilherme.roosr@gmail.com>
2025-02-19 06:21:01 -0300

    feat: add controller and route for listing users by building

--- src/api/company/listForSelect/list.routes.ts
+++ src/api/company/listForSelect/list.routes.ts
@@ -3,8 +3,10 @@ import { Router } from 'express';
 
 // CONTROLLERS
 import { listBuildingsForSelectController } from './controllers/listBuildingsForSelectController';
+import { listUsersForSelectController } from './controllers/listUsersForSelectController';
 
 // ROUTES
 export const listForSelectRouter = Router();
 
 listForSelectRouter.get('/buildings', listBuildingsForSelectController);
+listForSelectRouter.get('/users', listUsersForSelectController);
