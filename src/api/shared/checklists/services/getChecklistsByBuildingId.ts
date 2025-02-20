f5994ed0d3c42e1870c087f5ffe154afa4358ca3
Guilherme Roos <guilherme.roosr@gmail.com>
2025-02-20 18:01:29 -0300

    feat: refactor getChecklists service to require company ID and add new service for retrieving checklists by building ID

---
+++ src/api/shared/checklists/services/getChecklistsByBuildingId.ts
@@ -0,0 +1,37 @@
+import { prisma } from '../../../../../prisma';
+
+interface IGetChecklistsByBuildingId {
+  companyId: string;
+  buildingId: string[] | undefined;
+  userId?: string[] | undefined;
+}
+
+export async function getChecklistsByBuildingId({
+  companyId,
+  buildingId,
+  userId,
+}: IGetChecklistsByBuildingId) {
+  const checklists = await prisma.checklist.findMany({
+    include: {
+      checklistItem: true,
+      user: true,
+      images: true,
+    },
+
+    where: {
+      buildingId: {
+        in: buildingId,
+      },
+
+      building: {
+        companyId,
+      },
+
+      userId: {
+        in: userId,
+      },
+    },
+  });
+
+  return checklists;
+}
