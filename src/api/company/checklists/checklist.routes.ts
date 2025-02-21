406e527928710fd42265a4baecb633e9051f781d
Guilherme Roos <guilherme.roosr@gmail.com>
2025-02-20 22:22:17 -0300

    feat: add update checklist endpoint to checklist router

--- src/api/company/checklists/checklist.routes.ts
+++ src/api/company/checklists/checklist.routes.ts
@@ -15,6 +15,7 @@ import { createChecklistController2 } from '../../shared/checklists/controllers/
 import { getChecklistsController } from '../../shared/checklists/controllers/getChecklistsController';
 import { deleteChecklistController2 } from '../../shared/checklists/controllers/deleteChecklistController2';
 import { getChecklistsByBuildingIdController } from '../../shared/checklists/controllers/getChecklistsByBuildingIdController';
+import { updateChecklistController2 } from '../../shared/checklists/controllers/updateChecklistController2';
 
 export const checklistRouter: Router = Router();
 
@@ -31,6 +32,7 @@ checklistRouter.post('/v2/', createChecklistController2);
 
 checklistRouter.post('/', createChecklistController);
 
+checklistRouter.put('/v2/:checklistId', updateChecklistController2);
 checklistRouter.delete('/v2/:checklistId/:mode', deleteChecklistController2);
 
 // Esse report é do relatório
