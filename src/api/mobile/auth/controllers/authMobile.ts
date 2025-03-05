ea57f7858fad71e9ec4606e7b924a518f47594e2
Guilherme Roos <guilherme.roosr@gmail.com>
2025-03-05 17:43:45 -0300

    feat: implement mobile authentication controller

--- src/api/mobile/auth/controllers/authMobile.ts
+++ src/api/mobile/auth/controllers/authMobile.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from 'express';
 
-import { AuthMobile } from '../services/authMobile';
+import { TokenServices } from '../../../../utils/token/tokenServices';
+import { AuthServices } from '../services/authServices';
 import { PermissionServices } from '../../../shared/permissions/permission/services/permissionServices';
 
 import { needExist } from '../../../../utils/newValidator';
 
 // Instância do serviço
-const authMobile = new AuthMobile();
+const authServices = new AuthServices();
 const permissionServices = new PermissionServices();
+const tokenServices = new TokenServices();
 
-export async function listBuildingsFromResponsible(req: Request, res: Response) {
+export async function authMobile(req: Request, res: Response) {
   try {
     // Obtém o phoneNumber da query string
     const { login, password } = req.body;
@@ -19,14 +21,25 @@ export async function listBuildingsFromResponsible(req: Request, res: Response) 
       { label: 'Senha', variable: password },
     ]);
 
-    const user = await authMobile.canLogin({ login, password });
+    const user = await authServices.canLogin({ login, password });
 
     await permissionServices.checkPermission({
       UserPermissions: user.Permissions,
       permissions: ['admin:company', 'access:company'],
     });
 
-    return res.status(200).json({ user });
+    const isCompanyOwner = await authServices.isCompanyOwner({
+      userId: user.id,
+      companyId: user.Companies[0].Company.id,
+    });
+
+    const authToken = tokenServices.generate({
+      tokenData: {
+        userId: user.id,
+      },
+    });
+
+    return res.status(200).json({ authToken, user: { isCompanyOwner, ...user } });
   } catch (error) {
     return res.status(500).json({ error: 'Erro ao buscar os prédios do responsável' });
   }
