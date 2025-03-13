3d5a51aee1eff70f0d4e1bff0ceb2e4404e931c0
Guilherme Roos <guilherme.roosr@gmail.com>
2025-03-13 09:45:57 -0300

    feat: enhance deleteChecklistTemplateController with error handling and validation

--- src/api/shared/checklists/controllers/deleteChecklistTemplateController.ts
+++ src/api/shared/checklists/controllers/deleteChecklistTemplateController.ts
@@ -8,11 +8,35 @@ import { checkValues } from '../../../../utils/newValidator';
 export async function deleteChecklistTemplateController(req: Request, res: Response) {
   const { checklistTemplateId } = req.params as any as { checklistTemplateId: string };
 
-  checkValues([{ label: 'ID da checklist', type: 'string', value: checklistTemplateId }]);
+  checkValues([
+    { label: 'ID do template de checklist', type: 'string', value: checklistTemplateId },
+  ]);
 
   const checklistTemplate = await getChecklistTemplateById({ checklistId: checklistTemplateId });
 
-  await deleteChecklistTemplate({ where: { id: checklistTemplate?.id } });
+  if (!checklistTemplate) {
+    return res
+      .status(404)
+      .json({ ServerMessage: { message: 'Template de checklist não encontrado.' } });
+  }
 
-  return res.status(200).json({ ServerMessage: { message: 'Checklist excluído com sucesso.' } });
+  try {
+    await deleteChecklistTemplate({ checklistTemplateId: checklistTemplate.id });
+
+    return res
+      .status(200)
+      .json({ ServerMessage: { message: 'Template de checklist excluído com sucesso.' } });
+  } catch (error: any) {
+    if (error.code === 'P2003') {
+      return res.status(409).json({
+        ServerMessage: {
+          message: 'Não foi possível excluir, há checklists vinculados a esse template.',
+        },
+      });
+    }
+
+    return res
+      .status(500)
+      .json({ ServerMessage: { message: 'Erro ao excluir o template de checklist.' } });
+  }
 }
