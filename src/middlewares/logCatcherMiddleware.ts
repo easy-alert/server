403307fc439cef2dc57d3f1ac93e6543fef13fcb
Guilherme Roos <guilherme.roosr@gmail.com>
2025-03-20 16:18:47 -0300

    feat: enhance logCatcherMiddleware to validate Bearer token format and handle optional userId extraction

--- src/middlewares/logCatcherMiddleware.ts
+++ src/middlewares/logCatcherMiddleware.ts
@@ -12,17 +12,19 @@ import type { IToken } from './types';
 // eslint-disable-next-line consistent-return
 export const logCatcherMiddleware = async (req: Request, _res: Response, next: NextFunction) => {
   try {
+    const { authorization } = req.headers;
     const { body, method, params, query, originalUrl } = req;
 
-    const { authorization } = req.headers;
     let userId;
 
-    if (authorization) {
+    if (authorization && authorization.includes('Bearer') && !authorization.includes('null')) {
       const [, token] = authorization.split(' ');
+
       const secret: any = process.env.JWT_SECRET;
 
       const decoded = verify(token, secret);
-      userId = (decoded as IToken).userId;
+
+      userId = (decoded as IToken)?.userId;
     }
 
     if (!['POST', 'PUT', 'DELETE'].includes(method)) return next();
@@ -43,4 +45,3 @@ export const logCatcherMiddleware = async (req: Request, _res: Response, next: N
     next(error);
   }
 };
-
