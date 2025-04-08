a684770644a94df2e9627b4fa03bdefb486304fa
Guilherme Roos <guilherme.roosr@gmail.com>
2025-04-08 15:30:23 -0300

    feat: change updateMaintenanceHistory to use MaintenanceHistoryUncheckedUpdateInput type

--- src/api/shared/maintenanceHistory/services/updateMaintenanceHistory.ts
+++ src/api/shared/maintenanceHistory/services/updateMaintenanceHistory.ts
@@ -2,7 +2,7 @@ import { prisma, prismaTypes } from '../../../../../prisma';
 
 export async function updateMaintenanceHistory({
   ...data
-}: prismaTypes.MaintenanceHistoryUpdateInput) {
+}: prismaTypes.MaintenanceHistoryUncheckedUpdateInput) {
   return prisma.maintenanceHistory.update({
     where: {
       id: data.id as string,
