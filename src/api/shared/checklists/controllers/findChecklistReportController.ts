600df81e59b4d02a3d6715c1bad68fe717adf5a6
Guilherme Roos <guilherme@easyalert.com.br>
2025-05-09 19:23:11 -0300

    refactor: simplify findChecklistReportController by removing unused code and improving query handling

--- src/api/shared/checklists/controllers/findChecklistReportController.ts
+++ src/api/shared/checklists/controllers/findChecklistReportController.ts
@@ -1,12 +1,10 @@
 import { Response, Request } from 'express';
-import { ChecklistStatusName } from '@prisma/client';
+
+import type { ChecklistStatusName } from '@prisma/client';
+
 import { checklistServices } from '../services/checklistServices';
-import {
-  formatMonthYear,
-  setToUTCLastMinuteOfDay,
-  setToUTCMidnight,
-} from '../../../../utils/dateTime';
-import { checkDateRanges, checkValues } from '../../../../utils/newValidator';
+
+import { changeUTCTime } from '../../../../utils/dateTime';
 import { hasAdminPermission } from '../../../../utils/permissions/hasAdminPermission';
 import { handlePermittedBuildings } from '../../../../utils/permissions/handlePermittedBuildings';
 
@@ -17,86 +15,59 @@ export interface IParsedFilters {
   endDate: string;
 }
 
-interface IName {
-  name: string;
-}
-interface IImage {
-  name: string;
-  url: string;
-}
-
-interface ISeparateByMonth {
-  id: string;
-  name: string;
-  description: string | null;
-  date: Date;
-  building: IName;
-  syndic: IName | null;
-  frequency: number | null;
-  status: ChecklistStatusName;
-  observation: string | null;
-  images: IImage[];
-  detailImages: IImage[];
-}
-
-function separateByMonth(array: ISeparateByMonth[]) {
-  const separatedByMonth: { [key: string]: ISeparateByMonth[] } = {};
-
-  array.forEach((data) => {
-    const monthYear = `${data.date.getMonth() + 1}-${data.date.getFullYear()}`;
-
-    if (!separatedByMonth[monthYear]) {
-      separatedByMonth[monthYear] = [];
-    }
-
-    separatedByMonth[monthYear].push(data);
-  });
-
-  const result = Object.keys(separatedByMonth).map((key) => ({
-    month: formatMonthYear(key),
-    data: separatedByMonth[key],
-  }));
-
-  return result;
+interface IQuery {
+  buildingsId: string;
+  status: string;
+  startDate: string;
+  endDate: string;
+  page: string;
+  take: string;
 }
 
 export async function findChecklistReportController(req: Request, res: Response) {
-  const companyId = req.Company.id;
-  const { filters } = req.query as any as { filters: string };
-
-  const parsedFilters = JSON.parse(filters) as IParsedFilters;
-
-  checkValues([
-    { label: 'Data inicial', type: 'date', value: parsedFilters.startDate },
-    { label: 'Data final', type: 'date', value: parsedFilters.endDate },
-    { label: 'Edificações', type: 'array', value: parsedFilters.buildingNames, required: false },
-    { label: 'Status', type: 'array', value: parsedFilters.statusNames, required: false },
-  ]);
-
-  checkDateRanges([
-    { endDate: parsedFilters.endDate, startDate: parsedFilters.startDate, label: 'Datas' },
-  ]);
-
-  await checklistServices.checkAccessByCompany({ companyId });
+  const { buildingsId, status, startDate, endDate, page, take } = req.query as unknown as IQuery;
+  const { Company } = req;
 
   const isAdmin = hasAdminPermission(req.Permissions);
-  const permittedBuildings = handlePermittedBuildings(req.BuildingsPermissions, 'name');
+  const permittedBuildingsIds = isAdmin
+    ? undefined
+    : handlePermittedBuildings(req.BuildingsPermissions, 'id');
 
-  if (parsedFilters.buildingNames?.length === 0) {
-    parsedFilters.buildingNames = isAdmin ? undefined : permittedBuildings;
-  }
+  const companyIdFilter = Company ? Company?.id : undefined;
+
+  const buildingsIdFilter = !buildingsId ? permittedBuildingsIds : buildingsId.split(',');
+  const statusFilter =
+    typeof status === 'string' && status !== ''
+      ? (status.split(',') as ChecklistStatusName[])
+      : undefined;
+
+  const startDateFilter = startDate
+    ? changeUTCTime(new Date(String(startDate)), 0, 0, 0, 0)
+    : undefined;
+  const endDateFilter = endDate
+    ? changeUTCTime(new Date(String(endDate)), 23, 59, 59, 999)
+    : undefined;
+
+  await checklistServices.checkAccessByCompany({ companyId: Company.id });
 
   const checklists = await checklistServices.findManyForReport({
-    ...parsedFilters,
-    companyId,
-    startDate: setToUTCMidnight(new Date(parsedFilters.startDate)),
-    endDate: setToUTCLastMinuteOfDay(new Date(parsedFilters.endDate)),
+    buildingId: buildingsIdFilter,
+    companyId: companyIdFilter,
+    statusNames: statusFilter,
+    startDate: startDateFilter,
+    endDate: endDateFilter,
+    page: Number(page),
+    take: Number(take),
   });
 
   const completedCount = checklists.filter((e) => e.status === 'completed').length;
   const pendingCount = checklists.filter((e) => e.status === 'pending').length;
+  const inProgressCount = checklists.filter((e) => e.status === 'inProgress').length;
 
-  const checklistsForPDF = separateByMonth(checklists);
-
-  return res.status(200).json({ checklists, completedCount, pendingCount, checklistsForPDF });
+  return res.status(200).json({
+    checklists,
+    pendingCount,
+    inProgressCount,
+    completedCount,
+  });
 }
