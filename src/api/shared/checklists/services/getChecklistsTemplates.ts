ae92faf3ca5033d354c4a10fd79e0a8cce5522b5
Guilherme Roos <guilherme@easyalert.com.br>
2025-05-12 17:38:24 -0300

    feat: refactor getChecklistsTemplates to include companyId and remove building validation

--- src/api/shared/checklists/services/getChecklistsTemplates.ts
+++ src/api/shared/checklists/services/getChecklistsTemplates.ts
@@ -1,37 +1,19 @@
 import { prisma } from '../../../../../prisma';
 
 interface IGetChecklistsTemplates {
-  buildingId: string;
+  companyId: string;
+  buildingId?: string;
 }
 
-export async function getChecklistsTemplates({ buildingId }: IGetChecklistsTemplates) {
-  let building = null;
-
-  if (buildingId.length === 12) {
-    building = await prisma.building.findUnique({
-      where: {
-        nanoId: buildingId,
-      },
-    });
-  } else {
-    building = await prisma.building.findUnique({
-      where: {
-        id: buildingId,
-      },
-    });
-  }
-
-  if (!building) {
-    throw new Error('Edificação não encontrada.');
-  }
-
+export async function getChecklistsTemplates({ companyId, buildingId }: IGetChecklistsTemplates) {
   const checklistTemplates = await prisma.checklistTemplate.findMany({
     include: {
       items: true,
     },
 
     where: {
-      buildingId: building.id,
+      companyId,
+      buildingId,
     },
   });
 
