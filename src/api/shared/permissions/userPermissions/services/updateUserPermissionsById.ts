b22086631c6df83da8b1c1a362f91068b20776d1
Guilherme Roos <guilherme@easyalert.com.br>
2025-05-16 11:59:03 -0300

    feat: add companyId to IUpdateUserPermissionsById interface and update delete/create logic

--- src/api/shared/permissions/userPermissions/services/updateUserPermissionsById.ts
+++ src/api/shared/permissions/userPermissions/services/updateUserPermissionsById.ts
@@ -6,22 +6,26 @@ interface IUserPermissions {
 }
 
 interface IUpdateUserPermissionsById {
+  companyId: string;
   userId: string;
   userPermissions: IUserPermissions[];
 }
 
 export async function updateUserPermissionsById({
+  companyId,
   userId,
   userPermissions,
 }: IUpdateUserPermissionsById) {
   await prisma.userPermissions.deleteMany({
     where: {
+      companyId,
       userId,
     },
   });
 
   await prisma.userPermissions.createMany({
     data: userPermissions.map((userPermission) => ({
+      companyId,
       userId,
       permissionId: userPermission.id,
     })),
