6ee1b5c2179269c9c1c42488383a649cb4fb64b6
Guilherme Roos <guilherme@easyalert.com.br>
2025-05-28 15:38:39 -0300

    feat: format checklistUsers in getChecklistsController response

--- src/api/shared/checklists/controllers/getChecklistsController.ts
+++ src/api/shared/checklists/controllers/getChecklistsController.ts
@@ -18,5 +18,21 @@ export async function getChecklistsController(req: Request, res: Response) {
 
   const checklists = await getChecklists({ companyId: Company.id, checklistId });
 
-  return res.status(201).json(checklists);
+  const formattedChecklists = checklists.map((checklist) => {
+    const { checklistUsers, ...rest } = checklist;
+
+    const formattedChecklistUsers = checklistUsers.map((checklistUser) => ({
+      id: checklistUser.user.id,
+      name: checklistUser.user.name,
+      image: checklistUser.user.image,
+      email: checklistUser.user.email,
+    }));
+
+    return {
+      ...rest,
+      checklistUsers: formattedChecklistUsers,
+    };
+  });
+
+  return res.status(201).json(formattedChecklists);
 }
