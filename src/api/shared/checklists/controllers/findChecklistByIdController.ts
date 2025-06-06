cf06f1892a890af65a68d2e4650e0e34ecab5efd
Guilherme Roos <guilherme@easyalert.com.br>
2025-06-06 18:00:43 -0300

    refactor: enhance checklist ID validation and access check in findChecklistByIdController

--- src/api/shared/checklists/controllers/findChecklistByIdController.ts
+++ src/api/shared/checklists/controllers/findChecklistByIdController.ts
@@ -1,21 +1,17 @@
 import { Response, Request } from 'express';
+
 import { checklistServices } from '../services/checklistServices';
+
 import { checkValues } from '../../../../utils/newValidator';
-import { ServerMessage } from '../../../../utils/messages/serverMessage';
 
 export async function findChecklistByIdController(req: Request, res: Response) {
   const { checklistId } = req.params as any as { checklistId: string };
 
-  checkValues([{ label: 'ID da checklist', type: 'string', value: checklistId }]);
+  checkValues([{ label: 'ID da checklist', type: 'string', value: checklistId, required: true }]);
+
+  await checklistServices.checkChecklistAccess({ checklistId });
 
   const checklist = await checklistServices.findById(checklistId);
-
-  if (!checklist.building.Company?.canAccessChecklists) {
-    throw new ServerMessage({
-      statusCode: 403,
-      message: `Sua empresa não possui acesso a este módulo.`,
-    });
-  }
 
   return res.status(200).json({ checklist });
 }
