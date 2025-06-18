47b1e865c15ba42bf6d9f640805b598fc07b6f41
Marcelli <marcelli@easyalert.com.br>
2025-06-18 14:59:03 -0300

    feat: adds route to search for details of a building

--- src/api/backoffice/buildings/building.routes.ts
+++ src/api/backoffice/buildings/building.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 
 // FUNCTIONS
 import { listBuildings } from './controllers/listBuildings';
+import { listBuildingsDetails } from './controllers/listBuildingsDetails';
 
 // ROUTES
 export const buildingRouter = Router();
@@ -10,3 +11,4 @@ export const buildingRouter = Router();
 // BUILDING
 
 buildingRouter.get('/list', listBuildings);
+buildingRouter.get('/list/details/:buildingId', listBuildingsDetails);
