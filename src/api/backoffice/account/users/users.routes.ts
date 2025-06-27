e85659701c65d3735ccdbf966f731ea9b5d4f762
Marcelli <marcelli@easyalert.com.br>
2025-06-27 11:31:10 -0300

    feat: new route to edit user

--- src/api/backoffice/account/users/users.routes.ts
+++ src/api/backoffice/account/users/users.routes.ts
@@ -4,9 +4,12 @@ import { Router } from 'express';
 // FUNCTIONS
 import { listUsers } from './controllers/listUsers';
 import { listUserDetails } from './controllers/listUserDetails';
+import { editUserController } from './controllers/editUserController';
 
 // ROUTES
 export const usersRouter = Router();
 
 usersRouter.get('/list', listUsers);
 usersRouter.get('/details/:userId', listUserDetails);
+
+usersRouter.put('/editUser', editUserController);
