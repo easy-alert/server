eb5308cb50c6c53da4da7105fdee40585c67354e
Guilherme Roos <guilherme@easyalert.com.br>
2025-07-01 17:40:04 -0300

    Refactor createUserPermissions function for improved readability

--- src/api/shared/users/userPermission/services/createUserPermissions.ts
+++ src/api/shared/users/userPermission/services/createUserPermissions.ts
@@ -4,7 +4,9 @@ interface ICreateUserPermissions {
   data: prismaTypes.UserPermissionsCreateArgs;
 }
 
-export async function createUserPermissions<T>({ data }: ICreateUserPermissions): Promise<T | null> {
+export async function createUserPermissions<T>({
+  data,
+}: ICreateUserPermissions): Promise<T | null> {
   const result = await prisma.userPermissions.create(data);
 
   if (!result) {
