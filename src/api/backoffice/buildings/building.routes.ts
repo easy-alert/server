fec30478418ba50677ee42f7b120bb5014a63e30
Marcelli <marcelli@easyalert.com.br>
2025-07-02 09:01:38 -0300

    feat: new route to edit building

--- src/api/backoffice/buildings/building.routes.ts
+++ src/api/backoffice/buildings/building.routes.ts
@@ -4,11 +4,13 @@ import { Router } from 'express';
 // FUNCTIONS
 import { listBuildings } from './controllers/listBuildings';
 import { getBuildingDetails } from './controllers/getBuildingDetails';
+import { editBuildingController } from './controllers/editBuildingController';
 
 // ROUTES
 export const buildingRouter = Router();
 
 // BUILDING
-
 buildingRouter.get('/list', listBuildings);
 buildingRouter.get('/list/details/:buildingId', getBuildingDetails);
+
+buildingRouter.put('/edit/:buildingId', editBuildingController);
