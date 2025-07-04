0a2c5e1630e005c3a01b66830fe805587cb2a4d0
Marcelli <marcelli@easyalert.com.br>
2025-07-04 11:24:48 -0300

    feat: new route to block/activate building

--- src/api/backoffice/buildings/building.routes.ts
+++ src/api/backoffice/buildings/building.routes.ts
@@ -5,6 +5,7 @@ import { Router } from 'express';
 import { listBuildings } from './controllers/listBuildings';
 import { getBuildingDetails } from './controllers/getBuildingDetails';
 import { editBuildingController } from './controllers/editBuildingController';
+import { changeIsBlockedBuilding } from './controllers/changeIsBlockedBuilding';
 
 // ROUTES
 export const buildingRouter = Router();
@@ -14,3 +15,4 @@ buildingRouter.get('/list', listBuildings);
 buildingRouter.get('/list/details/:buildingId', getBuildingDetails);
 
 buildingRouter.put('/edit/:buildingId', editBuildingController);
+buildingRouter.put('/changeIsBlockedBuilding', changeIsBlockedBuilding);
