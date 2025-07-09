2183737770b8a3d51c9a3825049580c26f3c4407
Marcelli <marcelli@easyalert.com.br>
2025-07-09 09:38:27 -0300

    feat: new route

--- src/api/backoffice/buildings/building.routes.ts
+++ src/api/backoffice/buildings/building.routes.ts
@@ -4,7 +4,9 @@ import { Router } from 'express';
 // FUNCTIONS
 import { listBuildings } from './controllers/listBuildings';
 import { getBuildingDetails } from './controllers/getBuildingDetails';
+
 import { editBuildingController } from './controllers/editBuildingController';
+import { getBuildingTypes } from './controllers/getBuildingTypes';
 
 // ROUTES
 export const buildingRouter = Router();
@@ -12,5 +14,6 @@ export const buildingRouter = Router();
 // BUILDING
 buildingRouter.get('/list', listBuildings);
 buildingRouter.get('/list/details/:buildingId', getBuildingDetails);
+buildingRouter.get('/types/list', getBuildingTypes);
 
 buildingRouter.put('/edit/:buildingId', editBuildingController);
