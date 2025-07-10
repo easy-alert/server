3b6093329052ce1b12d793a931fc7db9203a4ed2
Marcelli <marcelli@easyalert.com.br>
2025-07-10 13:30:48 -0300

    fix: standardizes controller

--- src/api/backoffice/account/users/controllers/changeIsBlockedUser.ts
+++ src/api/backoffice/account/users/controllers/changeIsBlockedUser.ts
@@ -1,20 +1,42 @@
+// TYPES
 import { Request, Response } from 'express';
+
+// SERVICES
 import { userServices } from '../services/userServices';
+
+// CLASS
+import { Validator } from '../../../../../utils/validator/validator';
+
+const validator = new Validator();
 
 export async function changeIsBlockedUser(req: Request, res: Response) {
   const { userId } = req.body;
-  if (!userId) return res.status(400).json({ message: 'ID do usuário é obrigatório.' });
 
   try {
-    const updatedUser = await userServices.changeIsBlocked({ userId });
+    validator.check([{ label: 'ID do usuário', type: 'string', variable: userId }]);
+  } catch (validationError: any) {
+    return res.status(400).json({
+      ServerMessage: {
+        statusCode: 400,
+        message: validationError.message || 'ID do usuário é obrigatório.',
+      },
+    });
+  }
+
+  try {
+    await userServices.changeIsBlocked({ userId });
     return res.status(200).json({
-      updatedUser,
       ServerMessage: {
         statusCode: 200,
-        message: `Status alterado com sucesso.`,
+        message: 'Status alterado com sucesso.',
       },
     });
   } catch (error) {
-    return res.status(500).json({ message: 'Erro ao alterar status.' });
+    return res.status(500).json({
+      ServerMessage: {
+        statusCode: 500,
+        message: 'Erro ao alterar status.',
+      },
+    });
   }
 }
