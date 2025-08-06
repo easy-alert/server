e30bcaa5b786786699e01480742742b1ff118ee8
Marcelli <marcelli@easyalert.com.br>
2025-08-06 09:57:15 -0300

    feat: corrected file name

--- src/api/company/calendarCalled/controllers/listCalendarTickets.ts
+++ src/api/company/calendarCalled/controllers/listCalendarTickets.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import type { Request, Response } from 'express';
 
 import { checkValues } from '../../../../utils/newValidator/checkValues';
 import { getCalendarEvents } from '../services/getCalendarEvents';
@@ -6,15 +6,20 @@ import { getCalendarEvents } from '../services/getCalendarEvents';
 export async function listCalendarTickets(req: Request, res: Response) {
   const companyId = req.query.companyId as string;
   const year = Number(req.params.year);
-  const buildingId = req.query.buildingId as string | undefined;
+  const month = Number(req.query.month);
+
+  const buildingIds = req.query.buildingId
+    ? (req.query.buildingId as string).split(',').filter(Boolean)
+    : undefined;
 
   checkValues([
     { label: 'ID da empresa', type: 'string', value: companyId },
     { label: 'Ano', type: 'int', value: year },
+    { label: 'Mês', type: 'int', value: month },
   ]);
 
   try {
-    const { buildings, Days } = await getCalendarEvents({ companyId, year, buildingId });
+    const { buildings, Days } = await getCalendarEvents({ companyId, year, month, buildingIds });
 
     return res.status(200).json({
       ServerMessage: {
