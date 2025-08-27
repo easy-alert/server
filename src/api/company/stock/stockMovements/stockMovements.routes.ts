7b621a3e9b45bbf79091140f2f4c916b5dfe41d6
Guilherme Roos <guilherme@easyalert.com.br>
2025-08-27 04:14:35 -0300

    feat: add createStockMovementsController, api to company and transferStockMovement business rule

--- src/api/company/stock/stockMovements/stockMovements.routes.ts
+++ src/api/company/stock/stockMovements/stockMovements.routes.ts
@@ -3,8 +3,10 @@ import { Router } from 'express';
 
 // CONTROLLERS
 import { getStockMovementsController } from '../../../shared/stockMovement/controllers/getStockMovementsController';
+import { createStockMovementsController } from '../../../shared/stockMovement/controllers/createStockMovementsController';
 
 // ROUTES
 export const stockMovementsRouter = Router();
 
 stockMovementsRouter.get('/', getStockMovementsController);
+stockMovementsRouter.post('/', createStockMovementsController);
