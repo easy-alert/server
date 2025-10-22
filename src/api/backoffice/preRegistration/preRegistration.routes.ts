cc8e189a34e5f0de42c37e0763a7f852e0d83c32
Marcelli <marcelli@easyalert.com.br>
2025-10-22 13:54:35 -0300

    feat(SE-814): route for data filling by the user

--- src/api/backoffice/preRegistration/preRegistration.routes.ts
+++ src/api/backoffice/preRegistration/preRegistration.routes.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
+
 import { createPreRegistration } from './controllers/createPreRegistrationController';
+import { preRegistrationDetailsClient } from './controllers/preRegistrationDetailsClientController';
 
 export const preRegistrationRouter = Router();
 
 preRegistrationRouter.post('/invite', createPreRegistration);
+
+preRegistrationRouter.get('/details/:token', preRegistrationDetailsClient);
